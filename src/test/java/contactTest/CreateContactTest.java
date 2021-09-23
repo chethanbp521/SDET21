b6f03dd5d42810a3e128017a710eb80bc37c7d78
chethanbp521 <chethanbp521@gmail.com>
2021-09-23 14:54:07 +0530

    Parameters

--- src/test/java/contactTest/CreateContactTest.java
+++ src/test/java/contactTest/CreateContactTest.java
@@ -7,7 +7,13 @@ public class CreateContactTest
 	@Test(groups= "SmokeTest")
 	public void createContactTest()
 	{
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+				
 		System.out.println("Execute Create Contact Test");
+		System.out.println("-------------------"+BROWSER);
+		System.out.println("---------------"+URL);
+		
 	}
 	
 	@Test(groups= "RegressionTest")
