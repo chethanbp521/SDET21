15f5c439bae157da599ad05d786153ffae4111cd
chethanbp521 <chethanbp521@gmail.com>
2021-09-23 15:03:17 +0530

    commit

--- src/test/java/contactTest/CreateContactTest.java
+++ src/test/java/contactTest/CreateContactTest.java
@@ -19,19 +19,37 @@ public class CreateContactTest
 	@Test(groups= "RegressionTest")
 	public void createContactwithOrg()
 	{
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		
 		System.out.println("Execute Create Contact with Org");
+		
+		System.out.println("-------------------"+BROWSER);
+		System.out.println("---------------"+URL);
 	}
 	
 	@Test(groups= "RegressionTest")
 	public void deleteContact()
 	{
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		
 		System.out.println("Execute Delete Contact");
+
+		System.out.println("-------------------"+BROWSER);
+		System.out.println("---------------"+URL);
 	}
 	
 	@Test(groups= "RegressionTest")
 	public void modifyContact()
 	{
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		
 		System.out.println("Execute Modify Contact");
+		
+		System.out.println("-------------------"+BROWSER);
+		System.out.println("---------------"+URL);
 	}
 
 }
